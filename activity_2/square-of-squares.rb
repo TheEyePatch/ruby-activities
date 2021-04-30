4651f0e9e9bdde5fca355cbd1ec481406b236438
Leif Bersamina <leifbersamina@gmail.com>
2021-04-30 22:35:55 +0800

    add square-of-squares.rb

--- activity_2/square-of-squares.rb
+++ activity_2/square-of-squares.rb
@@ -1,15 +1,41 @@
 
-def square (var)
-    num =1
-    while num <= var
-        if num**2 == var
-            return true
-        elsif num**2 > var
-           return false
-        elsif num**2 < var
-            num+=1
-        end
-    end
+# def square (var)
+#     num =1
+#     while num <= var
+#         if num**2 == var
+#             return true
+#         elsif num**2 > var
+#            return false
+#         elsif num**2 < var
+#             num+=1
+#         end
+#     end
     
+# end
+# p square 33
+def square (var)
+    if var > 0
+        num =1
+        while num <= var
+            if num**2 == var
+                return true
+            elsif num**2 > var
+               return false
+            elsif num**2 < var
+                num+=1
+            end
+        end
+    elsif var === 0
+        return true
+    else 
+        return false
+    end
+   
 end
-p square 100000
+# p square 1000
+# p square 25
+# p square 26
+# p square 33
+# p square 1
+# p square 100
+# p square 0
