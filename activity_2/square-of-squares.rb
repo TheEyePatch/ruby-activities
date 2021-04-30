05cb6d58cd3fb2cfcef97ad24c0398f96046cf14
Leif Bersamina <leifbersamina@gmail.com>
2021-04-30 21:20:43 +0800

    add activity 2

---
+++ activity_2/square-of-squares.rb
@@ -0,0 +1,31 @@
+
+def square (var)
+    if var > 0
+        array = []
+        array_var = var.to_s.split(//)
+        array_var.each{|item| array.push(item.to_i)}
+        if array.sum > 9
+            return square (array.sum)
+        else
+            check_square (array.sum) 
+        end
+    else
+        false
+    end
+end
+
+def check_square (array)
+    case array
+        when 1
+            return true
+        when 4
+            return true
+        when 7
+            return true
+        when 9
+            return true
+        else false
+    end
+end
+
+puts square 100
