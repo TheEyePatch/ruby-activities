971fce419158447fad819835c183019397016f81
Leif Bersamina <leifbersamina@gmail.com>
2021-05-01 16:06:48 +0800

    add control-flow-task-1.rb

---
+++ activity_3/control-flow-task-1.rb
@@ -0,0 +1,16 @@
+arr = [1,3,5,6,9,11]
+number = 3
+
+def check_arr (array, int)
+    for num in array
+        if num === int
+            return true
+    
+        end
+    end
+    if num != int
+        return false
+    end
+end
+
+p check_arr arr, number
