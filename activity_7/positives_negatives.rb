e1a3e651681c2d87207b9b89c48e7e1991a1590f
Leif Bersamina <leifbersamina@gmail.com>
2021-05-10 12:57:53 +0800

    update positives_negatives.rb

--- activity_7/positives_negatives.rb
+++ activity_7/positives_negatives.rb
@@ -1,19 +1,20 @@
 arr = [1,2,3,4,5,6,7,8,9,10,-11,-12,-13,-14, -15]
-array =[]
-def check_intergers(arr)
+array =[1]
+def count_positives_sum_negatives(lst)
+    #your code here
     counter = 0
     negatives = []
     output = []
-    arr.each do |item| 
+    lst.each do |item| 
         if item > 0
             counter +=1
-            output[0] = counter
-        elsif item < 0
+        elsif item <= 0
             negatives.push(item)
-            output[1] = negatives.sum
         end
+        output[0] = counter
+        output[1] = negatives.sum        
     end
     return output
 end
-p check_intergers(arr)
-p check_intergers(array)
+p count_positives_sum_negatives(arr)
+p count_positives_sum_negatives(array)
